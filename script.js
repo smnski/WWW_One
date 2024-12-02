6172f985727df0afcf03e8341571166a7b886c0c
avren <szymonurbanski217@gmail.com>
2024-12-02 04:50:52 +0200

    Who uses var anymore

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var modal = document.getElementById("contactModal");
-    var contactButton = document.querySelector(".contact-button");
-    var closeButton = document.querySelector(".close-btn");
+    let modal = document.getElementById("contactModal");
+    let contactButton = document.querySelector(".contact-button");
+    let closeButton = document.querySelector(".close-btn");
 
-    // Show the modal when "Contact me" is clicked
     contactButton.addEventListener("click", function(event) {
-        event.preventDefault(); // Prevent default link behavior
+        event.preventDefault();
         modal.style.display = "block";
     });
 
-    // Close the modal when the close button is clicked
     closeButton.addEventListener("click", function() {
         modal.style.display = "none";
     });
 
-    // Close the modal if the user clicks outside of the modal content
     window.addEventListener("click", function(event) {
         if (event.target === modal) {
             modal.style.display = "none";
